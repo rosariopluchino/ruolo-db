c7bfecd9273ba67d62503b49a8fab5686cff8efd
rosario.pluchino <rosario.pluchino@aitho.it>
2025-10-29 15:27:52 +0100

    feat: aggiunta la classe TestStorico

---
+++ src/main/java/com/TestStorico.java
@@ -0,0 +1,20 @@
+package main.java.com;
+
+import java.sql.Date;
+
+import main.java.dao.StoricoDaoJdbc;
+import main.java.model.Storico;
+
+public class TestStorico {
+
+    public static void main(String[] args) {
+        Storico storico = new Storico();
+
+        Date now = new Date(System.currentTimeMillis());
+        storico.setDataInizio(now);
+        storico.setDataFine(now);
+
+        StoricoDaoJdbc storicoDaoJdbc = new StoricoDaoJdbc();
+        storicoDaoJdbc.inserisci(storico);
+    }
+}
