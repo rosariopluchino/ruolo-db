1929dfc1644ec8d31b476f0dc2e85dba82858a53
rosario.pluchino <rosario.pluchino@aitho.it>
2025-10-29 15:28:59 +0100

    feat: aggiunto model Ruolo

---
+++ src/main/java/model/Ruolo.java
@@ -0,0 +1,24 @@
+package main.java.model;
+
+public class Ruolo {
+    String nome;
+    String ruolo;
+
+    public Ruolo() {}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+}
